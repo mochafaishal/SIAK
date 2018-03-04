71ebd97df67d5e2d53d87810053fd3b6fd745e96
Your Name <you@example.com>
2018-03-04 18:15:02 +0700

    commit

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,9 @@
 <script>
     var url = window.location.pathname;
     var filename = url.substring(url.lastIndexOf('/')+1);
-    $('.sidebar-menu a[href="'+ filename +'"]').addClass('active');
+    if (filename.substr(0, 8) != 'akun-kas' ) {
+    $('.sidebar-menu a[href*="'+ filename.substr(0, 11)  +'"]').addClass('active');
+    } else {
+    $('.sidebar-menu a[href*="'+ filename.substr(0, 8)  +'"]').addClass('active');
+    }
 </script>
