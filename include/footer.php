884dd11fbee45521b72121d6dbca4432017a1912
Your Name <you@example.com>
2018-03-04 18:25:55 +0700

    commit

--- include/footer.php
+++ include/footer.php
@@ -1,8 +1,13 @@
 <script>
     var url = window.location.pathname;
     var filename = url.substring(url.lastIndexOf('/')+1);
+
     if (filename.substr(0, 8) != 'akun-kas' ) {
+    if(filename.substr(0, 9) == 'reimburse' || filename.substr(0, 9) == 'pelanggan') {
+    $('.sidebar-menu a[href*="'+ filename.substr(0, 9)  +'"]').addClass('active');
+    } else {
     $('.sidebar-menu a[href*="'+ filename.substr(0, 11)  +'"]').addClass('active');
+    }
     } else {
     $('.sidebar-menu a[href*="'+ filename.substr(0, 8)  +'"]').addClass('active');
     }
